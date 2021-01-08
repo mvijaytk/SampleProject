626b0d398672efec2510aa89b734ae00bee99e66
Vijaykumar M <vijaykumarm@SSPL.COM>
2021-01-08 14:14:27 +0530

    Added for reset checking Signed-off-by: Vijaykumar M <vijaykumarm@SSPL.COM>

--- SimpleJava3.java
+++ SimpleJava3.java
@@ -5,3 +5,14 @@ for (int i = 0; i <=10; i++) {
 	}
 }
 }
+
+
+class SimpleJava3{
+public static void main(String args[]){
+for (int i = 0; i <=100; i++) {
+  System.out.println("Simple Java3");
+	}
+}
+}
+
+
