48b96a20c8bbd2df27757c0be92e97452ebdde3e
Vijaykumar M <vijaykumarm@SSPL.COM>
2021-01-08 14:18:02 +0530

    changed it for reset testing Signed-off-by: Vijaykumar M <vijaykumarm@SSPL.COM>

--- SimpleJava3.java
+++ SimpleJava3.java
@@ -7,12 +7,3 @@ for (int i = 0; i <=10; i++) {
 }
 
 
-class SimpleJava3{
-public static void main(String args[]){
-for (int i = 0; i <=100; i++) {
-  System.out.println("Simple Java3");
-	}
-}
-}
-
-
